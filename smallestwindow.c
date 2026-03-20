104f5ece25cf8ce803a4bfa7601f1da8d49eb9eb
169million <carsone2026@gmail.com>
2026-03-19 18:58:38 -0700

    being a goober and left the sw.c
    goober stuff

--- smallestwindow.c
+++
@@ -1,4 +0,0 @@
-//#include<SDL.h> -- Works but has exception errors
-//main(B){for(SDL_CreateWindow(0,210,0,0);B-256;)SDL_WaitEvent(&B);}
-#include<SDL.h>
-B[9];main(){for(SDL_CreateWindow(0,210,0,0);B[9]-256;)SDL_WaitEvent(&B[9]);}
